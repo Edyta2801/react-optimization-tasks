b1b2786ec3cad41df89c55422977f97afc7add16
Edyta2801 <edyta.szarowska@interia.pl>
2021-01-27 15:16:43 +0100

    add components

---
+++ src/components/UsersContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+import UsersList from "./UsersList";
+
+class UsersContainer extends Component {
+  state = { users: [] };
+
+
+fetchData=()=>{
+    fetch("https://randomuser.me/api/?results=10")
+    .then(response=>response.json())
+    .then(data=>this.setState({users:data.results}))
+    .catch(error=>console.error(error))
+}
+
+componentDidMount(){
+    this.fetchData();
+}
+ handleRefresh=()=>{
+     this.fetchData();
+ }
+render(){
+    const{users}=this.state;
+    return<UsersList users={users} onRefresh={this.handleRefresh}/>
+}
+
+}
+
+export default UsersContainer;
