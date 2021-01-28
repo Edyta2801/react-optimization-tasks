f952bbcce20639fc3f6f33f1904714fdabd925ff
Edyta2801 <edyta.szarowska@interia.pl>
2021-01-28 11:45:50 +0100

    add shouldComponentUpdate for class Component

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,34 @@
 import React from "react";
 
-function User({ user }) {
-  return (
-    <div className="users__user">
-      <div className="users__user-name">{user.name.first}</div>
-      <div className="users__user-surname">{user.name.last}</div>
-    </div>
-  );
+class User extends React.Component {
+
+shouldComponentUpdate(nextProps, nextState){
+if(this.props.user.login.uuid===nextProps.user.login.uuid)
+return false;
+
+return false;
+  }
+
+
+  render() {
+    const { user } = this.props;
+
+    return (
+      <div className="users__user">
+        <div className="users__user-name">{user.name.first}</div>
+        <div className="users__user-surname">{user.name.last}</div>
+      </div>
+    );
+  }
 }
+
+// function User({ user }) {
+//   return (
+//     <div className="users__user">
+//       <div className="users__user-name">{user.name.first}</div>
+//       <div className="users__user-surname">{user.name.last}</div>
+//     </div>
+//   );
+// }
 
 export default User;
